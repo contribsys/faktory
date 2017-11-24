ba97d96888fc9f2e1a6acdf78947489515e38a27
Mike Perham <mperham@gmail.com>
2017-11-24 13:38:46 -0800

    Implement automatic backup policy, fixes #106

---
+++ server/backup.go
@@ -0,0 +1,76 @@
+package server
+
+import (
+	"os"
+	"time"
+
+	"github.com/contribsys/faktory/storage"
+	"github.com/contribsys/faktory/util"
+)
+
+type backupPolicy struct {
+	*Server
+	frequency time.Duration
+	count     int
+}
+
+func newBackupPolicy(s *Server) *backupPolicy {
+	freak := backupFrequency()
+	util.Debugf("Backing up storage every %v", freak)
+	return &backupPolicy{
+		Server:    s,
+		frequency: freak,
+		count:     0,
+	}
+}
+
+func (bp *backupPolicy) Frequency() int64 {
+	return int64(bp.frequency.Seconds())
+}
+
+func (bp *backupPolicy) Name() string {
+	return "backup"
+}
+
+func (bp *backupPolicy) Execute() error {
+	bp.count++
+
+	util.Debug("Running automatic backup...")
+	err := bp.Server.Store().Backup()
+	if err != nil {
+		util.Error("BACKUP FAILED", err)
+		return err
+	}
+	err = bp.Server.Store().PurgeOldBackups(storage.DefaultKeepBackupsCount)
+	if err != nil {
+		util.Error("PURGE FAILED", err)
+		return err
+	}
+	return nil
+}
+
+func (bp *backupPolicy) Stats() map[string]interface{} {
+	return map[string]interface{}{
+		"count": bp.count,
+	}
+}
+
+func backupFrequency() time.Duration {
+	durs := os.Getenv("FAKTORY_BACKUP_FREQUENCY")
+	if durs == "" {
+		// by default, backup every hour
+		return time.Duration(1 * time.Hour)
+	}
+
+	dur, err := time.ParseDuration(durs)
+	if err != nil {
+		util.Warnf("Invalid backup duration: %s", err)
+		return time.Duration(1 * time.Hour)
+	}
+
+	if dur.Seconds() < 300 {
+		util.Warnf("Can't take a backup more than once every 5 minutes: %v", dur)
+		return time.Duration(5 * time.Minute)
+	}
+	return dur
+}
--- server/tasks.go
+++ server/tasks.go
@@ -126,6 +126,9 @@ func (s *Server) startTasks(waiter *sync.WaitGroup) {
 	ts.AddTask(15, &reservationReaper{s, 0})
 	// reaps workers who have not heartbeated
 	ts.AddTask(15, &beatReaper{s, 0})
+	// backup runner
+	policy := newBackupPolicy(s)
+	ts.AddTask(policy.Frequency(), policy)
 	ts.Run(waiter)
 	s.taskRunner = ts
 }

400e9c971b296874f7dded4b5493390c210b5cef
Mike Perham <mperham@gmail.com>
2018-11-07 08:35:32 -0800

    Rename API as its not Redis-specific

--- util/util_linux.go
+++ util/util_linux.go
@@ -16,7 +16,7 @@ func isTTY(fd int) bool {
 	return err == 0
 }
 
-func TuneRedis(cmd *exec.Cmd) {
+func EnsureChildShutdown(cmd *exec.Cmd) {
 	// This ensures that, on Linux, if Faktory panics, our Redis child process will immediately
 	// get a SIGTERM signal to shutdown.  No such feature on Darwin/BSD, Redis will orphan.
 	cmd.SysProcAttr = &syscall.SysProcAttr{

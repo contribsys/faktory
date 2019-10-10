c3ce015788efca3f2b8e2aca9cf9186d22b786ed
tylerb <tyler@stillwater.me>
2019-10-10 11:26:30 -0600

    Add custom option setters

--- client/job_test.go
+++ client/job_test.go
@@ -3,6 +3,7 @@ package client
 import (
 	"encoding/json"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -13,4 +14,16 @@ func TestJob(t *testing.T) {
 	data, err := json.Marshal(job)
 	assert.NoError(t, err)
 	assert.Contains(t, string(data), "retry")
+}
+
+func TestJobCustomOptions(t *testing.T) {
+	job := NewJob("yo", 1)
+	expiresAt := time.Now().Add(1 * time.Hour)
+	job.SetUniqueFor(100).
+		SetUniqueUntil(UniqueUntilStart).
+		SetExpiresAt(expiresAt)
+
+	assert.EqualValues(t, 100, job.Custom["unique_for"])
+	assert.EqualValues(t, UniqueUntilStart, job.Custom["unique_until"])
+	assert.EqualValues(t, expiresAt.Format(ISO8601), job.Custom["expires_at"])
 }

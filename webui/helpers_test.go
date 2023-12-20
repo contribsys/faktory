82d3a55977d161412665a94a6d83b35a3f1e2130
Ibrahim Awwal <ibrahim@volition.co>
2023-12-20 14:06:38 -0800

    Add test cases for ActionMailer wrapped jobs
    The Rails 5 case is probably not exactly correct, but the important
    thing for now is that it uses the correct wrapped job class.

--- webui/helpers_test.go
+++ webui/helpers_test.go
@@ -54,6 +54,58 @@ func makeActiveJob(jobType string) *client.Job {
 	return job
 }
 
+func makeActionMailerJob(jobType string, mailerClass string, mailerMethod string) *client.Job {
+	var payload = []byte(fmt.Sprintf(`
+{
+  "jid": "60259247a836111c54b5ddf7",
+  "queue": "default",
+  "jobtype": "ActiveJob::QueueAdapters::FaktoryAdapter::JobWrapper",
+  "args": [
+    {
+      "arguments": [
+        "%[2]s",
+        "%[3]s",
+        "deliver_now",
+        {
+          "_aj_ruby2_keywords": [
+            "args"
+          ],
+          "args": [
+            {
+              "_aj_globalid": "gid://app/User/1"
+            }
+          ]
+        }
+      ],
+      "enqueued_at": "2023-12-20T21:35:27.281042097Z",
+      "exception_executions": {},
+      "executions": 0,
+      "job_class": "%[1]s",
+      "job_id": "0667e00a-61e4-4936-af9f-8ba494c060a0",
+      "locale": "en",
+      "priority": null,
+      "provider_job_id": "60259247a836111c54b5ddf7",
+      "queue_name": "default",
+      "scheduled_at": null,
+      "timezone": "Pacific Time (US & Canada)"
+    }
+  ],
+  "created_at": "2023-12-20T21:35:27.281126796Z",
+  "enqueued_at": "2023-12-20T21:35:27.281128369Z",
+  "retry": 3,
+  "custom": {
+    "wrapped": "%[1]s"
+  }
+}
+`, jobType, mailerClass, mailerMethod))
+	job := &client.Job{}
+	err := json.Unmarshal(payload, job)
+	if err != nil {
+		panic(err)
+	}
+	return job
+}
+
 var testCases = []testJob{
 	{
 		name:     "simple",
@@ -64,6 +116,16 @@ var testCases = []testJob{
 		name:     "wrapped",
 		job:      makeActiveJob("FooJob"),
 		expected: "FooJob",
+	},
+	{
+		name:     "Rails 5.x mailer",
+		job:      makeActionMailerJob("ActionMailer::DeliveryJob", "UserMailer", "welcome"),
+		expected: "UserMailer#welcome",
+	},
+	{
+		name:     "Rails 6.x mailer",
+		job:      makeActionMailerJob("ActionMailer::MailDeliveryJob", "UserMailer", "welcome"),
+		expected: "UserMailer#welcome",
 	},
 }
 

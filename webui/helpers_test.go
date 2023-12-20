abc417bb865054cb22f4b67334b3a567b6447ce7
Ibrahim Awwal <ibrahim@volition.co>
2023-12-20 13:14:53 -0800

    Use a more realistic test job object
    I got this by calling json.Marshal on a job that was rendered in the
    Web UI.

--- webui/helpers_test.go
+++ webui/helpers_test.go
@@ -1,6 +1,8 @@
 package webui
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	"github.com/contribsys/faktory/client"
@@ -12,6 +14,46 @@ type testJob struct {
 	expected string
 }
 
+func makeActiveJob(jobType string) *client.Job {
+	var payload = []byte(fmt.Sprintf(`
+{
+  "jid": "bb2a34025e7ce72a064d10d6",
+  "queue": "default",
+  "jobtype": "ActiveJob::QueueAdapters::FaktoryAdapter::JobWrapper",
+  "args": [
+    {
+      "arguments": [
+        1, 2, 3
+      ],
+      "enqueued_at": "2023-12-20T20:36:09.423631911Z",
+      "exception_executions": {},
+      "executions": 0,
+      "job_class": "%[1]s",
+      "job_id": "b24332fc-49bb-4128-8c4c-230c47abea91",
+      "locale": "en",
+      "priority": null,
+      "provider_job_id": "bb2a34025e7ce72a064d10d6",
+      "queue_name": "default",
+      "scheduled_at": null,
+      "timezone": "Pacific Time (US & Canada)"
+    }
+  ],
+  "created_at": "2023-12-20T20:36:09.423723003Z",
+  "enqueued_at": "2023-12-20T20:36:09.423725007Z",
+  "retry": 3,
+  "custom": {
+    "wrapped": "%[1]s"
+  }
+}
+`, jobType))
+	job := &client.Job{}
+	err := json.Unmarshal(payload, job)
+	if err != nil {
+		panic(err)
+	}
+	return job
+}
+
 var testCases = []testJob{
 	{
 		name:     "simple",
@@ -20,7 +62,7 @@ var testCases = []testJob{
 	},
 	{
 		name:     "wrapped",
-		job:      client.NewJob("ActiveJob::QueueAdapters::FaktoryAdapter::JobWrapper", "[{job_class: 'FooJob', args: [1]}]"),
+		job:      makeActiveJob("FooJob"),
 		expected: "FooJob",
 	},
 }
